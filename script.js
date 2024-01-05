fd03e7c33ca9e17aad06b1372295c0720d9f9a46
Ruan-Amorim <150949063+Ruan-Amorim@users.noreply.github.com>
2024-01-05 17:33:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function ClickMenu() {
+    var navMenu = document.getElementById('NAV-MENU')
+    if (navMenu.style.display == 'none') {
+        navMenu.style.display = 'block'
+    }else{
+        navMenu.style.display = 'none'
+    }
+}
