2777c10c86cc42843be5b1bb617d7ecb6a5d16db
Ruan-Amorim <150949063+Ruan-Amorim@users.noreply.github.com>
2024-01-06 14:51:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function ClickMenu() {
         navMenu.style.display = 'none'
     }
 }
+function receita() {
+    var bloco1 = document.getElementById('web4-bloco1')
+    var bloco2 = document.getElementById('web4-bloco2')
+    var back = document.getElementById('web4')
+    if(bloco2.style.display == 'block') {
+        bloco2.style.display = 'none'
+        bloco1.style.display = 'block'
+        
+    }else {
+        bloco1.style.display = 'none'
+        bloco2.style.display = 'block'
+        
+    }
+        }
+            
